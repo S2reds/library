4229e6d7926975e866eb868f534957097d699207
Kyle <thevarsin@gmail.com>
2023-02-17 18:16:28 -0800

    Added styling and fixed removeButton issues

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-let myLibrary = [];
 
 function Book(title,author,pages,read) {
     this.title = title
@@ -7,6 +6,31 @@ function Book(title,author,pages,read) {
     this.pages = pages
     this.read = read
 }
+
+let myLibrary = [];
+
+// form control
+const NAMEBOOK = document.querySelector('#bookname')
+const AUTHOR = document.querySelector('#author')
+const PAGES = document.querySelector('#pages')
+const READ = document.querySelector('#read')
+const err = document.querySelector('#err')
+
+function formControl() {
+    if (NAMEBOOK.value === '') err.innerHTML = 'Missing Book Name'
+    else if (AUTHOR.value === '') err.innerHTML = 'Missing Author'
+    else if (PAGES.value === '') err.innerHTML = 'How Many Pages?'
+    else {
+        const formData = Object.fromEntries(new FormData(form).entries())
+        addBook(formData.bookname, formData.author, formData.pages, formData.read)
+        document.querySelector('#bookname').value = ''
+        document.querySelector('#author').value = ''
+        document.querySelector('#pages').value = ''
+        err.innerHTML = ''
+    }
+
+}
+
 
 
 function loadBooks() {
@@ -49,18 +73,19 @@ function addBook(title, author, pages, read) {
 
 function createBook(book) {
     const main = document.querySelector('.main')
-    let div = document.createElement('div')
+    const div = document.createElement('div')
     div.classList.add('card')
-    let h3 = document.createElement('h3')
+    const h3 = document.createElement('h3')
     h3.textContent = book.title
-    let p = document.createElement('p')
+    const p = document.createElement('p')
     p.innerHTML = `author: ${book.author} <br>
                     pages: ${book.pages} <br>
                     read: ${book.read}`
 
-    let remove = document.createElement('button')
+    const remove = document.createElement('button')
     remove.textContent = 'remove'
     remove.classList.add('remove')
+    remove.value = myLibrary.length -1
     div.appendChild(h3)
     div.appendChild(p)
     div.appendChild(remove)
@@ -71,14 +96,15 @@ window.addEventListener('click', (e) => {
     if (e.target.className === 'remove') {
         console.log(e.target.value)
         const butt = document.querySelectorAll('.remove')
-        butt.forEach(b => {
+        butt.forEach(b => {           
             if (b.value === e.target.value) {
-                let newLib = myLibrary.slice(e.target.value,1)
-                localStorage.setItem('books', JSON.stringify(newLib))
+                const num = b.value
+                myLibrary.splice(num,1)
+                localStorage.setItem('books', JSON.stringify(myLibrary))
             }
             loadBooks()
         })
-       
+     
     }
 })
 
