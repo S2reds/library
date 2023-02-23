7b3723e1f6b5d4b65ebdc08fe46ce8518c8fa4fb
Kyle <thevarsin@gmail.com>
2023-02-22 17:00:09 -0800

    Refactored factory functions into classes

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 
-function Book(title,author,pages,read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
 }
 
 let myLibrary = [];
@@ -16,95 +18,118 @@ const PAGES = document.querySelector('#pages')
 const READ = document.querySelector('#read')
 const err = document.querySelector('#err')
 
-function formControl() {
-    if (NAMEBOOK.value === '') err.innerHTML = 'Missing Book Name'
-    else if (AUTHOR.value === '') err.innerHTML = 'Missing Author'
-    else if (PAGES.value === '') err.innerHTML = 'How Many Pages?'
-    else {
-        const formData = Object.fromEntries(new FormData(form).entries())
-        addBook(formData.bookname, formData.author, formData.pages, formData.read)
-        document.querySelector('#bookname').value = ''
-        document.querySelector('#author').value = ''
-        document.querySelector('#pages').value = ''
-        err.innerHTML = ''
+class formControl {
+    get form() {
+        if (NAMEBOOK.value === '') return err.innerHTML = 'Missing Book Name'
+        else if (AUTHOR.value === '') return err.innerHTML = 'Missing Author'
+        else if (PAGES.value === '') return err.innerHTML = 'How Many Pages?'
+        else {
+            const formData = Object.fromEntries(new FormData(form).entries())
+            new addBook(formData.bookname, formData.author, formData.pages, formData.read).add
+            document.querySelector('#bookname').value = ''
+            document.querySelector('#author').value = ''
+            document.querySelector('#pages').value = ''
+            err.innerHTML = ''
+        }
     }
 
 }
 
 
-function loadBooks() {
-    const bookList = JSON.parse(localStorage.getItem('books'));
-    myLibrary = bookList;
-    const main = document.querySelector('.main')
-    main.innerHTML = ''
-
-    if (localStorage.getItem('books') === null) {
-        myLibrary = [];
-    } else {
-        for (let i = 0; i < myLibrary.length; i++) {
-            let div = document.createElement('div')
-            div.classList.add('card')
-            let h3 = document.createElement('h3')
-            h3.textContent = myLibrary[i].title
-            let p = document.createElement('p')
-            p.innerHTML = `author: ${myLibrary[i].author} <br>
-                             pages: ${myLibrary[i].pages} <br>
-                             read: ${myLibrary[i].read}`
-        
-            let remove = document.createElement('button')
-            remove.textContent = 'Remove'
-            remove.classList.add('remove')
-            let update = document.createElement('button')
-            update.classList.add('update')
-            update.textContent = "Read it!";
-            update.value = i;
-            let holder = document.createElement('div')
-            holder.classList.add('holder')
-            holder.appendChild(remove)
-            holder.appendChild(update)
-            remove.value = i;
-            div.appendChild(h3)
-            div.appendChild(p)
-            div.appendChild(holder)
-            main.appendChild(div)
+class loadBooks {
+    constructor() {
+        this.load();
     }
-   
+    load() {
+        const bookList = JSON.parse(localStorage.getItem('books'));
+        myLibrary = bookList;
+        const main = document.querySelector('.main')
+        main.innerHTML = ''
+    
+        if (localStorage.getItem('books') === null) {
+            myLibrary = [];
+        } else {
+            for (let i = 0; i < myLibrary.length; i++) {
+                let div = document.createElement('div')
+                div.classList.add('card')
+                let h3 = document.createElement('h3')
+                h3.textContent = myLibrary[i].title
+                let p = document.createElement('p')
+                p.innerHTML = `author: ${myLibrary[i].author} <br>
+                                 pages: ${myLibrary[i].pages} <br>
+                                 read: ${myLibrary[i].read}`
+            
+                let remove = document.createElement('button')
+                remove.textContent = 'Remove'
+                remove.classList.add('remove')
+                let update = document.createElement('button')
+                update.classList.add('update')
+                update.textContent = "Read it!";
+                update.value = i;
+                let holder = document.createElement('div')
+                holder.classList.add('holder')
+                holder.appendChild(remove)
+                holder.appendChild(update)
+                remove.value = i;
+                div.appendChild(h3)
+                div.appendChild(p)
+                div.appendChild(holder)
+                main.appendChild(div)
+        }
+       
+    }
 }}
 
-function addBook(title, author, pages, read) {
-    const newBook = new Book(title,author,pages, read)
-    myLibrary.push(newBook)
-    localStorage.setItem('books', JSON.stringify(myLibrary))
-    createBook(newBook);
+class addBook {
+    constructor(title, author, pages, read) {
+        this.newBook = new Book(title,author,pages, read);
+    }
+    get add() {  
+        return (
+            myLibrary.push(this.newBook),
+            localStorage.setItem('books', JSON.stringify(myLibrary)),
+            new createBook(this.newBook).create()
+        )
+    }
 }
 
-function createBook(book) {
-    const main = document.querySelector('.main')
-    const div = document.createElement('div')
-    div.classList.add('card')
-    const h3 = document.createElement('h3')
-    h3.textContent = book.title
-    const p = document.createElement('p')
-    p.innerHTML = `author: ${book.author} <br>
-                    pages: ${book.pages} <br>
-                    read: ${book.read}`
-
-    const remove = document.createElement('button')
-    remove.textContent = 'Remove'
-    remove.classList.add('remove')
-    remove.value = myLibrary.length -1
-    const update = document.createElement('button')
-    update.textContent = 'Read it!'
-    update.classList.add('update')
-    update.value = myLibrary.length -1
-    const buttonHold = document.createElement('div')
-    buttonHold.classList.add('holder')
-    buttonHold.appendChild(remove)
-    buttonHold.appendChild(update)
-    div.appendChild(h3)
-    div.appendChild(p)
-    div.appendChild(buttonHold)
-    main.appendChild(div)
+class createBook {
+    constructor(book) {
+        this.create();
+        this.book = book;
+    }
+    create() {
+        try {
+            const main = document.querySelector('.main')
+            const div = document.createElement('div')
+            div.classList.add('card')
+            const h3 = document.createElement('h3')
+            h3.textContent = this.book.title
+            const p = document.createElement('p')
+            p.innerHTML = `author: ${this.book.author} <br>
+                            pages: ${this.book.pages} <br>
+                            read: ${this.book.read}`
+        
+            const remove = document.createElement('button')
+            remove.textContent = 'Remove'
+            remove.classList.add('remove')
+            remove.value = myLibrary.length -1
+            const update = document.createElement('button')
+            update.textContent = 'Read it!'
+            update.classList.add('update')
+            update.value = myLibrary.length -1
+            const buttonHold = document.createElement('div')
+            buttonHold.classList.add('holder')
+            buttonHold.appendChild(remove)
+            buttonHold.appendChild(update)
+            div.appendChild(h3)
+            div.appendChild(p)
+            div.appendChild(buttonHold)
+            main.appendChild(div)
+        } catch(e) {
+          
+        }
+    }
 }   
 
 window.addEventListener('click', (e) => {
@@ -117,7 +142,7 @@ window.addEventListener('click', (e) => {
                 myLibrary.splice(num,1)
                 localStorage.setItem('books', JSON.stringify(myLibrary))
             }
-            loadBooks()
+            new loadBooks().load()
         })
     }
     // update read status
@@ -130,10 +155,10 @@ window.addEventListener('click', (e) => {
                 else if (myLibrary[num].read === 'unread') myLibrary[num].read = 'read'
                 localStorage.setItem('books', JSON.stringify(myLibrary))
             }
-            loadBooks()
+            new loadBooks().load()
         })
     }
 })
 
 
-loadBooks()
+new loadBooks().load()
